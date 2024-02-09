eb3e96fb1996d0e375f83e8267bf8e82649f69c7
BadhonAhmad <nobelbadhon61@gmail.com>
2024-02-09 09:20:06 +0600

    fourth commit

--- crud.js
+++ crud.js
@@ -1,40 +1,67 @@
-const db = require('./database')
+const { query } = require('express');
+const db = require('./database');
 
-const createItem = (id, title, author, genre, price,callback) =>{
-    const sql = `INSERT INTO mock (id, title, author, genre, price) VALUES(?,?,?,?,?)`
-    db.run(sql, [id, title, author, genre, price],callback)
-}
+const createItem = (id, title, author, genre, price, callback) => {
+    const sql = `INSERT INTO mock (id, title, author, genre, price) VALUES (?, ?, ?, ?, ?)`;
+    db.run(sql, [id, title, author, genre, price], callback);
+};
 
-const updateItem = (id, title, author, genre, price,callback) =>{
-    const nam = `SELECT title FROM mock WHERE id = ?`;
-    db.get(nam,[id],(err,row)=>{
-        if(err){
+const updateItem = (id, title, author, genre, price, callback) => {
+    const selectQuery = `SELECT title FROM mock WHERE id = ?`;
+    const updateQuery = `UPDATE mock SET title = ?, author = ?, genre = ?, price = ? WHERE id = ?`;
+
+    db.get(selectQuery, [id], (err, row) => {
+        if (err) {
             return callback(err);
         }
-        if(!row){
-            return callback(new Error(`book with id: ${id} was not found`))
+        if (!row) {
+            return callback(new Error(`Book with id: ${id} was not found`));
         }
-        const sql = `UPDATE mock  SET title = ?, author = ?,genre = ?, price = ? WHERE id = ?`
-        db.run(sql, [title, author, genre, price, id], callback)
-    })   
-}
 
-const getSingleItem = (id, callback) =>{
-    const nam = `SELECT * FROM mock WHERE id = ?`;
-    db.get(nam,[id],(err,row)=>{
-        if(err){
-            return callback(err);
-        }
-        if(!row){
-            return callback(new Error(`book with id: ${id} was not found`));
-        }
-        const sql = `SELECT * FROM mock WHERE id = ?`
-        db.get(sql,[id],callback);
-    })
-}
+        db.run(updateQuery, [title, author, genre, price, id], callback);
+    });
+};
+
+const getSingleItem = (id, callback) => {
+    const sql = `SELECT * FROM mock WHERE id = ?`;
+    db.get(sql, [id], callback);
+};
 
 const getAllBooks = (callback) => {
-    const data = `SELECT * FROM mock`
-    db.all(data,[],callback);
-}
-module.exports = {createItem,updateItem,getSingleItem,getAllBooks}
+    const sql = `SELECT * FROM mock`;
+    db.all(sql, [], callback);
+};
+
+
+const searchBook = (queryPar, callback) => {
+    let query = `SELECT * FROM mock`;
+
+    if (queryPar.title) {
+        query += ` WHERE title = '${queryPar.title}'`;
+    } else if (queryPar.author) {
+        query += ` WHERE author = '${queryPar.author}'`;
+    } else if (queryPar.genre) {
+        query += ` WHERE genre = '${queryPar.genre}'`;
+    }
+
+    if (queryPar.sort) {
+        query += ` ORDER BY ${queryPar.sort}`;
+    } else {
+        query += ` ORDER BY id`;
+    }
+
+    if (queryPar.order) {
+        query += ` ${queryPar.order}`;
+    } else {
+        query += ` ASC`;
+    }
+
+    db.all(query, callback);
+};
+
+// const deleteBook = (id, callback) => {
+//     const sql = 'DELETE FROM mock WHERE id = ?';
+//     db.run(sql, [id], callback);
+// };
+
+module.exports = { createItem, updateItem, getSingleItem, getAllBooks, searchBook };
--- index.js
+++ index.js
@@ -1,57 +1,65 @@
 const express = require('express');
-const {itemsClear,createItem, getAllBooks, updateItem, deleteItem,getSingleItem} = require('./crud');
+const { searchBook, createItem, getAllBooks, updateItem, getSingleItem } = require('./crud');
 
 const app = express();
 
 app.use(express.json());
 
 const port = 5000;
-app.listen(port,()=> console.log(`listening to port ${port}`));
-//add books
-app.post('/api/books',(req, res) =>{
-    const {id, title, author, genre, price} = req.body
-    createItem(id, title, author, genre, price, (err) =>{
-        if(err){
-            res.status(500).send(err.message);            
-        }else{
-            res.status(201).send({id, title, author, genre, price})
-        }
-    })
-})
-//update books with error message
-app.put('/api/books/:id',(req,res)=>{
-    const id = req.params.id;
-    const {title, author, genre, price} = req.body;
-    updateItem(id,title,author,genre,price,(err)=>{
-        if(err){
-            res.status(404).send({
-                message : err.message
-            });
-        }
-        else{
-            res.status(200).send({id,title,author,genre,price});
-        }
-    })
-})
+app.listen(port, () => console.log(`Listening to port ${port}`));
 
-//get a single book
-app.get('/api/books/:id',(req,res)=>{
-    getSingleItem(req.params.id,(err,row) => {
-        if(err){
-            res.status(500).send({
-                message : err.message
-            });
+//Add books
+app.post('/api/books', (req, res) => {
+    const { id, title, author, genre, price } = req.body;
+    createItem(id, title, author, genre, price, (err) => {
+        if (err) {
+            res.status(500).send(err.message);
+        } else {
+            res.status(201).send({ id, title, author, genre, price });
         }
-        else{
-            res.status(200).send(row);
-        }
-    }); 
+    });
 });
 
+//Update books with error message
+app.put('/api/books/:id', (req, res) => {
+    const id = req.params.id;
+    const { title, author, genre, price } = req.body;
+    updateItem(id, title, author, genre, price, (err) => {
+        if (err) {
+            res.status(404).send({
+                message: err.message
+            });
+        } else {
+            res.status(200).send({ id, title, author, genre, price });
+        }
+    });
+});
 
-app.get('/api/books',(req,res) =>{
-    getAllBooks((err,row)=>{
-        res.status(200).send({books:row});
-    })
-})
-//
+//Get a single book
+app.get('/api/books/:id', (req, res) => {
+    getSingleItem(req.params.id, (err, row) => {
+        if (err) {
+            res.status(500).send({
+                message: err.message
+            });
+        } else {
+            res.status(200).send(row);
+        }
+    });
+});
+
+app.get('/api/books', (req, res) => {
+    getAllBooks((err, rows) => {
+        res.status(200).send({ books: rows });
+    });
+});
+
+app.get('/api/book', (req, res) => {
+    const queryPar = req.query;
+    searchBook(queryPar, (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(200).json({ books: rows });
+    });
+});
